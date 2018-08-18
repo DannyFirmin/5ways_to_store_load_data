5313832af5415150475f9a202cac10a675ebc6cc
Danny Firmin <u6611178@anu.edu.au>
2018-08-18 15:48:34 +1000

    Finished JSON

--- ShoeSize.java
+++ ShoeSize.java
@@ -3,12 +3,11 @@
  * This class stores a persons shoe size.
  */
 
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.*;
 
 public class ShoeSize {
     private static final String SHOESIZEENAME = "SHOESIZE";
@@ -44,35 +43,32 @@ public class ShoeSize {
 
     static ShoeSize load() {
         // add code here that will load shoe size from a file called "FILENAME"
+        // JSON
+        File f = new File(FILENAME);
+        ShoeSize res = new ShoeSize();
         try {
-            FileReader f_reader = new FileReader(FILENAME);
-            Scanner in = new Scanner(f_reader);
-            Integer mySize = in.nextInt();
-            in.close();
-            return new ShoeSize(mySize);
-
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
-            return new ShoeSize();
-        } catch (NoSuchElementException e){
-            System.err.format("NoSuchElementException: %s%n", e);
-            return new ShoeSize();
+            JSONObject obj = (JSONObject) JSONValue.parse(new FileReader(f));
+            res.shoesize = ((Long) obj.get(SHOESIZEENAME)).intValue();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        return res;
     }
 
     void save() {
         // add code here that will save shoe size into a file called "FILENAME"
+        // JSON
+
+        File f = new File(FILENAME);
+        JSONObject obj = new JSONObject();
+        obj.put(SHOESIZEENAME, shoesize);
+        FileWriter out;
         try {
-            FileWriter f_writer = new FileWriter(FILENAME);
-            BufferedWriter b_writer = new BufferedWriter(f_writer);
-            System.out.println(shoesize);
-            String info = shoesize.toString();
-            b_writer.write(info);
-            b_writer.close();
-
-
+            out = new FileWriter(f);
+            obj.writeJSONString(out);
+            out.close();
         } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
+            e.printStackTrace();
         }
     }
 }
